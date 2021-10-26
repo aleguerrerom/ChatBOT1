28d6f22e0a14fcf8e4b559ce9c22dfbea854d915
AG00051917 <alejandro.guerrero@icumed.com>
2021-10-26 15:14:41 -0600

    avance

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from nltk.chat.util import Chat, reflections
 
-mis_reflecciones = {
-    "ir":"fui",
-    "hola":"hey"
+# lista de reflecciones
+reflections = {
+    "yo":"tu",
+    "hola":"hey",
+    "yo soy": "tu eres"
 }
 
-###Array
-preguntas = [
+###Arreglo de pares
+pairs = [
     [
         r"mi nombre es (.*)",
         ["Hola %1, como estas?",]
@@ -16,16 +18,16 @@ preguntas = [
         ["Mi nombre es Chatbot!!!",]
     ],
     [
-        r"como estas",
-        ["Bien, y tu?",]
+        r"como estas|como vas",
+        ["Bien, y tu?", "Pura vida"]
     ],
     [
         r"hola|hey|buenas",
-        ["Hola", "Que tal",]
+        ["Hola", "Que tal", "Buenas"]
     ],
     [
         r"Que (.*) quieres?",
-        ["Nada Gracias", ]
+        ["Nada Gracias","Comer" ]
     ],
     [
         r"(.*) fuiste creado?",
@@ -33,18 +35,49 @@ preguntas = [
     ],
     [
         r"Finalizar|Fin|FIN|FINALIZAR|fin|finalizar",
-        ["Chao", "Fue un gusto hablar contigo" ]
+        ["Chao", "Fue un gusto hablar contigo","Adios"]
     ],
 ]
 
+
+## Funcion para pedir numero entero para el menu
+def pedirNumeroEntero():
+    correcto = False
+    num = 0
+    while (not correcto):
+        try:
+            num = int(input("Introduce un numero entero: "))
+            print("Elige una opcion: ")
+            print("1. Opcion Preguntas")
+            print("2. Salir")
+            correcto = True
+        except ValueError:
+            print('Error, introduce un numero entero')
+    return num
+
+#funcion de chateo
 def chatear():
-        print("Hola Bienvenid@!!")
-        chat = Chat(preguntas, mis_reflecciones)
-        chat.converse()
+        salir = False
+        while not salir:
+            opcion = pedirNumeroEntero()
+            if opcion == 1:
+                print("Hola Bienvenid@!!")
+                chat = Chat(pairs, reflections)
+                chat.converse()
+            elif opcion == 2:
+                    option = input("Deseas Salir S/N?")
+                    if option == 'S' or option == 's':
+                        salir = True
+                        print("Has salido del Chat")
+                        exit()
+                    else:
+                        salir = False
+                        pedirNumeroEntero()
+            else:
+                print("Introduce un numero entre 1 y 2")
+
 
 if __name__ == "__main__":
     chatear()
 
-while True:
-    if preguntas !
-    chatear()
+chatear()
